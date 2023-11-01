5a7688eb92ce994238cfc9e569f2eaa3f1f71c65
Pj2121jp <prasadpatil24212@gmail.com>
2023-11-01 18:58:58 +0530

    cmt

---
+++ string2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    char Arr[20];
+
+    printf("Enter your Name ==>> ");
+    scanf("%s",Arr);
+   // getc(Arr);
+
+    printf("Your name is : %s",Arr);
+
+    return 0;
+}
