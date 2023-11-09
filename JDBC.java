9ac832202ebda197f88849fa22e9c9734cf6e83e
Pj2121jp <prasadpatil24212@gmail.com>
2023-11-09 22:51:16 +0530

    cmt

--- JDBC.java
+++ JDBC.java
@@ -1,36 +1,28 @@
-import java.sql.'; // 1
+import java.sql.*; 
 
-class Database
+class JDBC
 {
+        public static void main(String arg[]) throws Exception 
+        {
 
-public static void main(String arg[]) throws Exception t
+                String URL = ("jdbc:mysql://localhost:3306/PPA");
 
-String URL = "jdbc:mysql://localhost:3306/ppa49";
+                String Username = "root";
+                String Password = "root";
+                String Query= "select from student";
 
-String Username = "root"; String Password = "root";
+                Connection cobj = DriverManager.getConnection (URL, Username, Password); 
+                Statement sobj = cobj.createStatement();
+                ResultSet robj = sobj.executeQuery (Query); 
 
-String Query= "select from student";
+        while(robj.next())
+        {
+            System.out.println("Roll_no"+robj.getInt("roll_no"));
+            System.out.println("Name: "+robj.getString("name"));
+            System.out.println("Address: "+robj.getString("address"));
+            System.out.println("Marks "+robj.getInt("marks"));
 
-Connection cobj = DriverManager.getConnection (URL, Username, Password); 1/2
+        }
+    }
+}
 
-Statement sobj = cobj.createStatement();
-
-ResultSet robj = sobj.executeQuery (Query); // 4
-
-while(rob).next())
-
-// 5
-
- System.out.println("RID: "+robj.getInt("rno")); System.out.println("Name: "+robj.getString("name")); System.out.println("City: "+rob).getString("city")); System.out.println("Marks "+robj.getInt("marks"));
-
-// execute()
-
-// executeQuery()
-
-// executeUpdate()
-
-Create table
-
-select
-
-delete / update / alter / insert
